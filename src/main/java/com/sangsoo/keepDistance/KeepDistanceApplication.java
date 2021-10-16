d726d9f7868ba7e98a317d1a3cd57643b6150c60
constantoh <ohconstant@naver.com>
2021-10-16 23:21:04 +0900

    Initial commit

---
+++ src/main/java/com/sangsoo/keepDistance/KeepDistanceApplication.java
@@ -0,0 +1,13 @@
+package com.sangsoo.keepDistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepDistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepDistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangsoo/keepDistance/KeepDistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangsoo.keepDistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepDistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
